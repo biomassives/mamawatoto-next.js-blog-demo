f7f39b91242c2f0e2a50e6b9a375cc0ffc2523e2
Greg Willson <greg@biomassiv.es>
2022-02-01 16:00:31 -0700

    update

--- components/intro.js
+++ components/intro.js
@@ -2,24 +2,33 @@ export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        Mamawatoto Eco School
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{" "}
+        One for the Environment, one for the future. - MC Teach{" "}
         <a
-          href="https://nextjs.org/"
+          href="/posts/addressing-east-africas-solid-waste-crisis"
           className="underline hover:text-success duration-200 transition-colors"
         >
-          Next.js
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://www.datocms.com/"
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          DatoCMS
+          Donate
         </a>
-        .
+        {" "}
+        or
+        {" "}
+        <a
+          href="/posts/buy-a-climate-nft"
+          className="underline hover:text-success duration-200 transition-colors"
+        >Buy
+          </a>
+        
+        {" / "}
+        <a
+          href="/posts/verify-a-climate-nft"
+          className="underline hover:text-success duration-200 transition-colors"
+        >
+          Verify</a>
+          {" "}
+        Climate Action NFTs.
       </h4>
     </section>
   );
