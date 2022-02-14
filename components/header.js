2e39194cb774eeae280b8f826c673dc40eef46a7
G. Willson <greg@biomassiv.es>
2022-02-13 19:08:40 -0700

    Update header.js

--- components/header.js
+++ components/header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <Link href="/" title="Mamawatoto Eco School">
-          <img src="https://www.datocms-assets.com/62560/1643741142-mamawa0.png" alt="Mamawatoto Eco School">
+          <img src="https://www.datocms-assets.com/62560/1643741142-mamawa0.png" alt="Mamawatoto Eco School" />
       </Link>
     </h2>
   );
